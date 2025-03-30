a61846db276012b3830cef04df9f1784e312aca2
withered <sergeyzezyanov1@gmail.com>
2025-03-31 02:33:08 +0700

    Добавлено превью обложек в админке

--- main/admin.py
+++ main/admin.py
@@ -1,28 +1,51 @@
 """For 'main' app's models registration"""
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . models import TextTitle, GraphicTitle, \
                         TextTitleChapter, GraphicTitleChapter, \
                         GraphicTitlePage
-
+                        
 
 # modifying admin site fields to show
 class TextTitleChapterInline(admin.TabularInline):
     model = TextTitleChapter
     extra = 1
+    
 class GraphicTitleChapterInline(admin.TabularInline):
     model = GraphicTitleChapter
     extra = 1
+    
 class GraphicTitlePageInline(admin.TabularInline):
     model = GraphicTitlePage
     extra = 1
+    
 class TextTitleAdmin(admin.ModelAdmin):
-    readonly_fields = ('added_at',)
+    readonly_fields = ('title_cover_preview', 'added_at')
     inlines = [TextTitleChapterInline]
+    
+    def title_cover_preview(self, title):
+        width = title.title_cover.width
+        height = title.title_cover.height
+        
+        return mark_safe(
+            f'<img src="{title.title_cover.url}" width="{width}px" height={height}px />'
+            )
+    
 class GraphicTitleAdmin(admin.ModelAdmin):
-    readonly_fields = ('added_at',)
-    inlines = [GraphicTitleChapterInline]
+    readonly_fields = ('title_cover_preview', 'added_at')
+    inlines = [GraphicTitleChapterInline]    
+    
+    def title_cover_preview(self, title):
+        width = title.title_cover.width
+        height = title.title_cover.height
+        
+        return mark_safe(
+            f'<img src="{title.title_cover.url}" width="{width}px" height={height}px />'
+            )
+        
 class TextTitleChapterAdmin(admin.ModelAdmin):
     readonly_fields = ('added_at',)
+    
 class GraphicTitleChapterAdmin(admin.ModelAdmin):
     readonly_fields = ('added_at',)
     inlines = [GraphicTitlePageInline]
