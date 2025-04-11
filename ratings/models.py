8d4ccf177d85052b217515997cea6066278e999b
withered <sergeyzezyanov1@gmail.com>
2025-04-11 17:22:00 +0700

    Добавлена модель под оценки тайтлов
    Это простая модель на основе 5-балльной оценки

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,46 @@
+"""Models for rating system"""
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from users.models import User
 
-# Create your models here.
+
+class TitleRating(models.Model):
+    rate = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    # for any title type
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveBigIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(check=models.Q(rate__range=(1, 5)), name='rate in range 1 to 5'),
+            models.UniqueConstraint(fields=['user', 'content_type'], name='rate only once')
+        ]
+        verbose_name = 'Оценка тайтла'
+        verbose_name_plural = 'Оценки тайтлов'
+        
+    def __str__(self):
+        return f'Оценки "{self.content_object}"'
+    
+    @staticmethod
+    def get_average_rate(title):
+        """Get average rate for title"""
+        avg = TitleRating.objects.filter(
+            content_type=ContentType.objects.get_for_model(title),
+            object_id=title.id
+        ).aggregate(models.Avg('rate'))
+        
+        return avg
+    
+    @staticmethod
+    def get_rates_count(title):
+        """Get amount of rates for title"""
+        count = TitleRating.objects.filter(
+            content_type=ContentType.objects.get_for_model(title),
+            object_id=title.id
+        ).count()
+        
+        return count
